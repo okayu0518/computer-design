c248fb16b8bc18b47c7ace46ec8ec74bcf1007da
okayu0518 <250376s@ugs.kochi-tech.ac.jp>
2025-06-27 06:25:42 +0900

    organized with gemini

--- verilog/src/CNT60.v
+++ verilog/src/CNT60.v
@@ -5,7 +5,6 @@ output [3:0] QL;
 output CA;
 reg [2:0] QH;
 reg [3:0] QL;
-reg CA10, CA6;
 wire EN10;
 
 assign EN10 = EN | INC;
@@ -15,27 +14,21 @@ always @(posedge CLK) begin
     if (CLR || RST) begin
         QL <= 4'b0;
 	QH <= 3'b0;
-        CA10 <= 1'b0;
-	CA6 <= 1'b0;
     end else if (EN10) begin
         if (QL >= 4'b1001) begin
             QL <= 4'b0;
-	    CA10 <= 1'b1;
 	    if (QH >= 3'b101) begin
 		    QH <= 3'b0;
-		    CA6 <= 1'b1;
 	    end else begin
 		    QH <= QH + 1'b1;
-		    CA6 <= 1'b0;
 	    end
         end else begin
             QL <= QL + 1'b1;
-            CA10 <= 1'b0;
         end
 	end
 end
 
-assign CA = (QL==4'd9) && (QH==3'd5) && EN10;
+assign CA = (QL==4'd9) & (QH==3'd5) & EN10;
 //assign CA = CA10 & CA6 & EN;
 
 endmodule
